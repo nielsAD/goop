a479e8144c4cbc85d67f972f91d5912755f10b6a
NielsAD <niels.a.d@gmail.com>
2019-03-11 16:29:03 +0100

    plugin: Add randkick plugin that demonstrates commands

---
+++ plugins/randkick.lua
@@ -0,0 +1,35 @@
+-- Author:  Niels A.D.
+-- Project: goop (https://github.com/nielsAD/goop)
+-- License: Mozilla Public License, v2.0
+--
+-- Add randkick command that randomly kicks a user from the channel when triggered
+-- 
+-- Options:
+--   AccessTrigger:  Access level required to trigger command
+--   AccessProtect:  Whitelist access level
+
+goop:AddCommand("randkick", command(function(trig, gw)
+    local lvl = options["AccessTrigger"] or access.Operator
+    if trig.User.Access < lvl then
+        return nil
+    end
+    
+    local chan = gw:ChannelUsers()
+    if chan == nil then
+        return nil
+    end
+
+    local users   = {}
+    local max_lvl = options["AccessProtect"] or access.Whitelist
+    for _, u in chan() do
+        if u.Access < max_lvl then
+            table.insert(users, u)
+        end
+    end
+
+    if #users == 0 then
+        return nil
+    end
+
+    return gw:Kick(users[math.random(#users)].ID)
+end))
