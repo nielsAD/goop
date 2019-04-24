68e828a50a953e9b69fc4a15473da299d1a7b57c
NielsAD <niels.a.d@gmail.com>
2019-04-24 07:44:55 +0200

    cmd: Sort output of .who on access level

--- goop/cmd/where.go
+++ goop/cmd/where.go
@@ -38,10 +38,41 @@ var plural = map[bool]string{
 	true:  "s",
 }
 
+type whoList struct {
+	name   []string
+	access []gateway.AccessLevel
+}
+
+func (l *whoList) Append(name string, access gateway.AccessLevel) {
+	l.name = append(l.name, name)
+	l.access = append(l.access, access)
+}
+
+func (l *whoList) String() string {
+	sort.Sort(l)
+	return strings.Join(l.name, " ")
+}
+
+func (l *whoList) Len() int {
+	return len(l.name)
+}
+
+func (l *whoList) Swap(i, j int) {
+	l.name[i], l.name[j] = l.name[j], l.name[i]
+	l.access[i], l.access[j] = l.access[j], l.access[i]
+}
+
+func (l *whoList) Less(i, j int) bool {
+	if l.access[i] == l.access[j] {
+		return strings.ToLower(l.name[i]) < strings.ToLower(l.name[j])
+	}
+	return l.access[i] > l.access[j]
+}
+
 // Execute command
 func (c *Who) Execute(t *gateway.Trigger, gw gateway.Gateway, g *goop.Goop) error {
 	var total = 0
-	var online = []string{}
+	var list whoList
 	for _, gw := range g.Gateways {
 		var users = gw.ChannelUsers()
 		if users == nil {
@@ -51,17 +82,17 @@ func (c *Who) Execute(t *gateway.Trigger, gw gateway.Gateway, g *goop.Goop) erro
 		total += len(users)
 
 		if len(users) >= 10 {
-			online = append(online, fmt.Sprintf("<%d in %s>", len(users), gw.Discriminator()))
+			list.Append(fmt.Sprintf("<%d in %s>", len(users), gw.Discriminator()), gateway.AccessMax)
 		} else {
 			for _, u := range users {
 				if u.Access >= gateway.AccessOperator {
-					online = append(online, fmt.Sprintf("[%s@%s]", u.Name, gw.Discriminator()))
-				} else {
-					online = append(online, fmt.Sprintf("%s@%s", u.Name, gw.Discriminator()))
+					list.Append(fmt.Sprintf("[%s@%s]", u.Name, gw.Discriminator()), u.Access)
+				} else if u.Access > gateway.AccessIgnore {
+					list.Append(fmt.Sprintf("%s@%s", u.Name, gw.Discriminator()), u.Access)
 				}
 			}
 		}
 	}
-	sort.Strings(online)
-	return t.Resp(fmt.Sprintf("%d user%s online: %s", total, plural[total != 1], strings.Join(online, " ")))
+
+	return t.Resp(fmt.Sprintf("%d user%s online: %s", total, plural[total != 1], list.String()))
 }
